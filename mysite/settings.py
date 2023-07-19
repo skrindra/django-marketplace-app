8749b165023f010e46e2d8541621ef1ac0d40d87
sivakumar <courses.skr@gmail.com>
2023-07-19 21:56:16 +0530

    include whitenoise middleware & collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,7 +140,7 @@ STATIC_URL = 'static/'
 # collect static files for deployment, this includes the 
 # default django css too
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
